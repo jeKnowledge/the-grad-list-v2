1e1061868a548ef03be5c2312802afc0f8c132b1
Teresa Salazar <teresa.sal13@gmail.com>
2015-11-20 10:42:40 +0000

    removed url on post_submit.js

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -2,7 +2,6 @@ Template.postSubmit.events({
 	'submit form': function(e) {
 	e.preventDefault();
 	var post = {
-		url: $(e.target).find('[name=url]').val(),
 		title: $(e.target).find('[name=title]').val(),
 		owner: Meteor.userId(),
 		username: Meteor.user().username || Meteor.user().profile.name,
