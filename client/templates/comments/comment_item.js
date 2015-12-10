f77e51ac0af5284440e1269dfeac74425e981ec2
Teresa Salazar <teresa.sal13@gmail.com>
2015-12-10 21:50:10 +0000

    added owner helper to comment_item.js

--- client/templates/comments/comment_item.js
+++ client/templates/comments/comment_item.js
@@ -1,6 +1,10 @@
 Template.commentItem.helpers({
     ownerUsername: function() {
         return Meteor.users.findOne({"_id": this.owner}).username || Meteor.users.findOne({"_id": this.owner}).profile.name;
+    },
+
+    owner: function() {
+    	return Meteor.userId() === this.owner;
     }
 }); 
 
