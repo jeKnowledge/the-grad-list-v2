621c76361229fd3513fcb267835f43c3fda5cb61
Teresa Salazar <teresa.sal13@gmail.com>
2015-12-22 19:30:57 +0000

    console.log errors in login

--- client/templates/home.js
+++ client/templates/home.js
@@ -1,10 +1,14 @@
 Template.home.events({
-
         'submit form': function(event){
         event.preventDefault();
         var username = $('[name=username]').val();
         var password = $('[name=password]').val();
-        Meteor.loginWithPassword(username, password);
-        Router.go('postsList');
+        Meteor.loginWithPassword(username, password, function(error){
+		    if(error){
+		        console.log(error.reason);
+		    } else {
+		        Router.go("postsList");
+		    }
+		});
     }
 });
