5d25fe4ecafa224362f42588491bde7c75096265
Teresa Salazar <teresa.sal13@gmail.com>
2015-12-22 16:33:35 +0000

    Created register.js

---
+++ client/templates/register.js
@@ -0,0 +1,12 @@
+Template.register.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var username = $('[name=username]').val();
+        var password = $('[name=password]').val();
+        Accounts.createUser({
+            username: username,
+            password: password
+        });
+        Router.go('postsList');
+    }
+});
