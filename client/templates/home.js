b4c2692f5c3517b4dc92ecaaf2e67a922048edc8
Teresa Salazar <teresa.sal13@gmail.com>
2015-12-26 16:22:49 +0000

    Updated home.js

--- client/templates/home.js
+++ client/templates/home.js
@@ -1,14 +1,5 @@
-Template.home.events({
-        'submit form': function(event){
-        event.preventDefault();
-        var username = $('[name=username]').val();
-        var password = $('[name=password]').val();
-        Meteor.loginWithPassword(username, password, function(error){
-		    if(error){
-		        console.log(error.reason);
-		    } else {
-		        Router.go("postsList");
-		    }
-		});
-    }
-});
+Template.home.helpers({
+  user: function(){
+      return Meteor.users.find();
+  }	
+})
