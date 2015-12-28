abb79e965c72a94992983debe5eaa6379e2699a8
Teresa Salazar <teresa.sal13@gmail.com>
2015-12-28 12:17:34 +0000

    Added service configuration to facebook

--- server/publications.js
+++ server/publications.js
@@ -15,3 +15,13 @@ Meteor.publish("usersData", function() {
     this.ready();
   }
 });
+
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+ 
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '795219033939224',
+    secret: 'ed6bce59cb18a9dc52810adf6fe74491'
+});
