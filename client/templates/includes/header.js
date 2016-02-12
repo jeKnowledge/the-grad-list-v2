3c6b60665455ab56b9b7e826f3703857f11a8c7f
Maria Margarida <mariamargaridals@gmail.com>
2016-02-12 23:16:13 +0000

    Navbar-search changes.

--- client/templates/includes/header.js
+++ client/templates/includes/header.js
@@ -1,19 +1,23 @@
 Template.header.helpers({
 
-	userID: function() {
-		return Meteor.userId();
-	},
+        userID: function() {
+            return Meteor.userId();
+        },
 
-	getUsername: function() {
-		return Meteor.user().username;
-	},
+        getUsername: function() {
+            return Meteor.user().username;
+        },
 
-	usersIndex: () => UsersIndex,
+        usersIndex: () => UsersIndex,
+
+    inputAttributes: function() {
+    return {placeholder: "Search clients, orders and suppliers...", class: 'form-control'};
+}
 });
 
 
 Template.header.events({
-	'click #logout': function(event){
+    'click #logout': function(event){
         event.preventDefault();
         Meteor.logout();
         Router.go('/');
