dc10a97306be84d5e20249c28bf51799fa8ffa94
teresalazar13 <teresa.sal13@gmail.com>
2016-03-30 19:09:02 +0100

    minor changes.

--- client/templates/pages/tags.js
+++ client/templates/pages/tags.js
@@ -1,6 +1,5 @@
 Template.tags.helpers({
     postsByTags: function() {
-    	console.log(this.tags);
-      return Posts.find({tags: { $regex: this.tags }});
+      return Posts.find({tags: this.tags });
     },
 });
