09496b5bb515b4caececd89c0c2b0bc3d09dc723
teresalazar13 <teresa.sal13@gmail.com>
2016-06-16 21:23:02 +0100

    facebook_image

--- server/Accounts.js
+++ server/Accounts.js
@@ -2,5 +2,6 @@ Accounts.onCreateUser(function(options, user) {
     user.follows = new Array();
     user.followed = new Array();
     user.medals = Number(0);
+    user.facebook_image = Number(1);
     return user;
 });
