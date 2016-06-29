9622b5b40da57043eb661bbeef688272e052baed
teresalazar13 <teresa.sal13@gmail.com>
2016-06-29 10:36:21 +0100

    update meteor

--- server/MeteorMethods.js
+++ server/MeteorMethods.js
@@ -4,13 +4,11 @@ Meteor.methods({
             fileName: Meteor.user().services.facebook.name,
             meta: {}
         });
-        console.log("http://graph.facebook.com/" + Meteor.user().services.facebook.id + "/picture/?type=large");
-        console.log(Images.collection.findOne({name: Meteor.user().services.facebook.name})._id);
-        var a = Images.collection.findOne({name: Meteor.user().services.facebook.name})._id;
+        var facebook_image = Images.collection.findOne({name: Meteor.user().services.facebook.name})._id;
         Meteor.users.update(Meteor.userId(), {
             $set: {
                 username: Meteor.user().services.facebook.name,
-                image: a
+                image: facebook_image
             }
         });
     },
