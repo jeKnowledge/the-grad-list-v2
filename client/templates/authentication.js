dc18cc7050c175ccc7f07270d6512354242e3b83
teresalazar13 <teresa.sal13@gmail.com>
2016-06-29 14:51:27 +0100

    fixed issue #6 - Automatically try to login when Enter key is pressed

--- client/templates/authentication.js
+++ client/templates/authentication.js
@@ -6,7 +6,14 @@ Template.authentication.events({
             Meteor.loginWithPassword($('#sign-in-tab').find('#username-input').val(), $('#sign-in-tab').find('#password-input').val(), function(err) {
                 if (err) {
                     if (err.reason === "Match failed") {
-                        console.log('Todo:fix this error');
+                        sAlert.error('Please enter valid username and password', {
+                            effect: 'slide',
+                            position: 'bottom-right',
+                            timeout: 'none',
+                            onRouteClose: false,
+                            stack: false,
+                            offset: '80px'
+                        });
                     } else {
                         sAlert.error('Username or password incorrect', {
                             effect: 'slide',
@@ -70,5 +77,57 @@ Template.authentication.events({
     'submit form': function(event) {
         event.preventDefault();
         console.log(currentTab);
+    },
+
+    'keypress input#password-input': function(evt, template) { //to do -> password input algo exists in sign up and enter shouldn't work there
+        if (evt.which === 13) {
+            Meteor.loginWithPassword($('#sign-in-tab').find('#username-input').val(), $('#sign-in-tab').find('#password-input').val(), function(err) {
+                if (err) {
+                    sAlert.error('Username or password incorrect', {
+                        effect: 'slide',
+                        position: 'bottom-right',
+                        timeout: 'none',
+                        onRouteClose: false,
+                        stack: false,
+                        offset: '80px'
+                    });
+                    console.log(err);
+                }
+            });
+        }
+    },
+
+    'keypress input#password-confirm-input': function(evt, template) {
+        if (evt.which === 13) {
+            var name = $('#sign-up-tab').find('#username-input').val();
+            Meteor.call('doesUserExist', name, function(error, result) {
+                if (result === true) {
+                    sAlert.error('Username already exists', {
+                        effect: 'slide',
+                        position: 'bottom-right',
+                        timeout: 'none',
+                        onRouteClose: false,
+                        stack: false,
+                        offset: '80px'
+                    });
+                }
+            });
+            var pass = $('#sign-up-tab').find('#password-input').val();
+            var confirm_pass = $('#sign-up-tab').find('#password-confirm-input').val();
+            if (pass === confirm_pass) {
+                Accounts.createUser({username: $('#sign-up-tab').find('#username-input').val(), password: $('#sign-up-tab').find('#password-input').val()});
+                Meteor.call("defaultPicture", Meteor.userId());
+            } else {
+                sAlert.error('Passwords do not match', {
+                    effect: 'slide',
+                    position: 'bottom-right',
+                    timeout: 'none',
+                    onRouteClose: false,
+                    stack: false,
+                    offset: '80px'
+                });
+            }
+        }
+        currentTab = 'sign_up';
     }
 });
