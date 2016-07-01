b9659ec8db715adca02e9a3eb22030a0e9f457de
teresalazar13 <teresa.sal13@gmail.com>
2016-07-01 19:13:27 +0100

    tag page checks if tag exists

--- client/templates/pages/tags.js
+++ client/templates/pages/tags.js
@@ -1,4 +1,13 @@
 Template.tags.helpers({
+    title: function() {
+        var url = Router.current().location.get().path.slice(6);
+        if (Posts.find({tags: url}).count() !== 0) {
+            return "[ Posts with the tag ]";
+        } else {
+            return "[ There are no posts with the tag ]";
+        }
+    },
+
     postsByTags: function() {
         var url = Router.current().location.get().path.slice(6);
         return Posts.find({tags: url});
