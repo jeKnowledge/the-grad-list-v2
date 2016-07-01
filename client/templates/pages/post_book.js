092a312bd73a71fcadfc08253209c53159262639
teresalazar13 <teresa.sal13@gmail.com>
2016-07-01 15:32:24 +0100

    fix issue #15 - in my book, when there are no posts the title is No challenges were completed

--- client/templates/pages/post_book.js
+++ client/templates/pages/post_book.js
@@ -1,4 +1,20 @@
 Template.postBook.helpers({
+    title: function() {
+        if (Posts.find({
+            $and: [
+                {
+                    owner: this._id
+                }, {
+                    completed: true
+                }
+            ]
+        }).count() !== 0) {
+            return "Completed posts by";
+        } else {
+            return "No challenges were completed by";
+        }
+    },
+
     postsCompleted: function() {
         return Posts.find({
             $and: [
