9c1623536451864f1314a07a549b6f0aa89096ac
teresalazar13 <teresa.sal13@gmail.com>
2017-02-11 13:00:55 +0000

    Adds rate limiting.

--- server/rateLimiting.js
+++ server/rateLimiting.js
@@ -1,24 +1,19 @@
-/*
-
-// Get list of all method names on Lists
-const LISTS_METHODS = _.pluck([
-  insert,
-  makePublic,
-  makePrivate,
-  updateName,
-  remove,
-], 'name');
-
-// Only allow 5 list operations per connection per second
-if (Meteor.isServer) {
+// Subscriptions
+for (var subscription in Meteor.default_server.publish_handlers) {
   DDPRateLimiter.addRule({
-    name(name) {
-      return _.contains(LISTS_METHODS, name);
-    },
-
-    // Rate limit per connection ID
-    connectionId() { return true; }
+    type:'subscription',
+    name:'subscription'
   }, 5, 1000);
 }
 
-*/
+// Methods
+for (var method in Meteor.default_server.method_handlers) {
+  DDPRateLimiter.addRule({
+    type:'method',
+    name:'method'
+  }, 5, 1000);
+}
+
+Accounts.validateLoginAttempt(function(attempt){
+  return true;
+});
